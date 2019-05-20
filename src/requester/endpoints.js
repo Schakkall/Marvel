6e6c7488343de59331862194f31693a54888fd72
RaZ <jeuriqueribeiro@gmail.com>
2019-05-20 12:21:31 -0300

    authorization

--- src/requester/endpoints.js
+++ src/requester/endpoints.js
@@ -1,6 +1,6 @@
 import hashIt from './md5';
 
-export const ALL_HEROES_URI = '';
-export const HERO_INFO_URI = '';
-export const HERO_PIC_URI = '';
+export const ALL_HEROES_URI = `https://gateway.marvel.com:443/v1/public/characters?apikey=${hashIt()}`;
+export const HERO_INFO_URI = (id) => `https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=${hashIt()}`;
+export const HERO_PIC_URI = (id) => `https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=${hashIt()}`;
 export const EXAMPLE_URI = 'https://randomuser.me/api';
