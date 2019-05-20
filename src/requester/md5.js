e0bd93c0d73f9d33aa15c0fac473015ae917b5c9
RaZ <jeuriqueribeiro@gmail.com>
2019-05-20 11:04:56 -0300

    authorization

--- src/requester/md5.js
+++ src/requester/md5.js
@@ -1,5 +1,7 @@
 import md5 from 'md5';
+const PRIVATE_KEY = '5c2338b9d9aedeb093f095d01517ccf576a7fe80';
+const PUBLIC_KEY = 'f9e95fa16ec3865d019d40ce464b7d82';
 
-function hashIt(ts, privateKey, publicKey) {
+function hashIt(ts, privateKey = PRIVATE_KEY, publicKey = PUBLIC_KEY) {
     return md5(ts+privateKey+publicKey);
 }
