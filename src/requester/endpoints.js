11c1ceaa0c3235d07bee49caec575686e044fd15
RaZ <jeuriqueribeiro@gmail.com>
2019-05-20 18:55:12 -0300

    Pagination

--- src/requester/endpoints.js
+++ src/requester/endpoints.js
@@ -1,6 +1,6 @@
 import hashIt from './md5';
 
-export const ALL_HEROES_URI = `https://gateway.marvel.com:443/v1/public/characters?apikey=${hashIt()}`;
+export const ALL_HEROES_URI = (limit = 10, offset) `https://gateway.marvel.com:443/v1/public/characters?limit=${limit}&offset=${offset}&apikey=${hashIt()}`;
 export const HERO_INFO_URI = (id) => `https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=${hashIt()}`;
 export const HERO_PIC_URI = (id) => `https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=${hashIt()}`;
 export const EXAMPLE_URI = 'https://randomuser.me/api';
