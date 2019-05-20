2607279477d0ace3c23705aa1fba53b15197680e
RaZ <jeuriqueribeiro@gmail.com>
2019-05-20 10:57:24 -0300

    EndPoints

---
+++ src/requester/endpoints.js
@@ -0,0 +1,4 @@
+export const ALL_HEROES_URI = '';
+export const HERO_INFO_URI = '';
+export const HERO_PIC_URI = '';
+export const EXAMPLE_URI = 'https://randomuser.me/api';
--- src/requester/requester.js
+++ src/requester/requester.js
@@ -1,6 +1,8 @@
+import * as endpoints from 'endpoints';
+
 export const fetchData = async () => {
   try {
-    const response = await fetch("https://randomuser.me/api");
+    const response = await fetch(endpoints.EXAMPLE_URI);
     const data = await response.json();
     return data;
   } catch (e) {
